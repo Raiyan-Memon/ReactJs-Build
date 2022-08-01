12ffe3f8bc531365e64697d4fc327e939cc1e347
Raiyan Memon <raiyanmemon7860@gmail.com>
2022-08-01 23:14:06 +0530

    Initial Commit

---
+++ precache-manifest.d2c1aac16d2962b88f4c0b9d74b8a5ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11d8a622b61fb81a0db",
+    "url": "/static/css/main.75e53860.chunk.css"
+  },
+  {
+    "revision": "c11d8a622b61fb81a0db",
+    "url": "/static/js/main.c11d8a62.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "97588d46235ec70cf1d7",
+    "url": "/static/js/2.97588d46.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "22f9978a555210b7c51ae7a137085a28",
+    "url": "/index.html"
+  }
+];
